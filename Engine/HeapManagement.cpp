74e28ac387571c96b57e79646a1cba7301139b06
Tezika <tezikazhou@gmail.com>
2018-10-09 16:24:38 -0600

    fix the build errors
    this error is caused by the order of "stdafx" header file. It seems very strange.

--- Engine/HeapManagement.cpp
+++ Engine/HeapManagement.cpp
@@ -1,3 +1,31 @@
-#include "HeapManager.h"
 #include "stdafx.h"
+#include "HeapManager.h"
+
+Engine::HeapManager::HeapManager()
+{
+}
+
+Engine::HeapManager::~HeapManager()
+{
+}
+
+void Engine::HeapManager::Destroy()
+{
+}
+
+void* Engine::HeapManager::Alloc(size_t i_size)
+{
+	return nullptr;
+}
+
+void* Engine::HeapManager::Alloc(size_t i_size, unsigned int i_alignment)
+{
+	return nullptr;
+}
+
+bool Engine::HeapManager::Free(void* i_ptr)
+{
+	return true;
+}
+
 
--- Engine/HeapManager.h
+++ Engine/HeapManager.h
@@ -1,1 +1,16 @@
 #pragma once
+
+namespace Engine
+{
+	class HeapManager
+	{
+	public:
+		HeapManager();
+		~HeapManager();
+		void* Alloc(size_t i_size);
+		void* Alloc(size_t i_size, unsigned int i_aligment);
+		bool Free(void* i_ptr);
+		void Destroy();
+	};
+
+}
