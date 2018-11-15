c3733d81f3707c5202d2ba19ce1bcbf9fba65fdc
Tezika <tezikazhou@gmail.com>
2018-11-15 11:44:09 -0700

    add the GameObject class into engine code.

---
+++ Engine/GameObject.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "GameObject.h"
+
+namespace Engine
+{
+	GameObject::GameObject()
+	{
+
+	}
+
+	GameObject::~GameObject()
+	{
+
+	}
+
+	GameObject::GameObject(const GameObject & i_other)
+	{
+
+	}
+
+	void GameObject::operator=(const GameObject & i_other)
+	{
+
+	}
+}
---
+++ Engine/GameObject.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Point2D.h"
+
+namespace Engine
+{
+	class GameObject
+	{
+	public:
+		GameObject();
+		GameObject(const GameObject &);
+		void operator= (const GameObject &);
+		~GameObject();
+
+		inline const Point2D<float> & GetPosition() { return m_position; }
+		inline void SetPosition(const Point2D<float>& i_other) { m_position = i_other; };
+
+		inline const Point2D<float> & GetDirection() { return m_direction; }
+		inline void SetDirection(const Point2D<float>& i_other) { m_direction = i_other; };
+
+	private:
+		Point2D<float> m_direction;
+		Point2D<float> m_position;
+	};
+}
