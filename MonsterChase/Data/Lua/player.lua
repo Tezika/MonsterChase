c8f9e3cc853a28669158ee887733da59e2b00670
Tezika <tezikazhou@gmail.com>
2019-02-24 14:49:44 -0700

    modify the lua file a little bit.

--- MonsterChase/Data/Lua/player.lua
+++ MonsterChase/Data/Lua/player.lua
@@ -1,8 +1,8 @@
 player = 
 {
     name = "Tezika",
-    initial_Position = {1.0, 2.0},
-    physics_setting = {
+    initial_position = {1.0, 2.0},
+    physics_settings = {
         mass = 1,
         force = {1.0, 1.0},
         dragness = 0.005
