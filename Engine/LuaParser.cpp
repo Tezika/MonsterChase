ad5a91af990fd050358a758153d008bfdba6a2cb
Tezika <tezikazhou@gmail.com>
2019-02-23 19:59:01 -0700

    Engine: Add the Lua parser in.

---
+++ Engine/LuaParser.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "LuaParser.h"
+#include "../Exports/Lua/lua.h"
+#include "GameObject.h"
+#include "SubSystems.h"
+#include "Assert.h"
+
+namespace Engine
+{
+	GameObject * CreateByaLuaFile( const char * i_fileName )
+	{
+		assert( i_fileName );
+		return nullptr;
+	}
+}
---
+++ Engine/LuaParser.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Engine
+{
+	class GameObject;
+	GameObject * CreateByaLuaFile( const char * i_fileName );
+}
