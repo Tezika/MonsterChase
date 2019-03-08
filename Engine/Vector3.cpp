65871d6afb0f9fd6d3a3a4b564cb79004cd45f33
Tezika <tezikazhou@gmail.com>
2019-03-07 17:09:40 -0700

    Engine: Finish the basic logic for vector3.

--- Engine/Vector3.cpp
+++ Engine/Vector3.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "Vector3.h"
+#include "Assert.h"
+#include "math.h"
 
 namespace Engine
 {
@@ -8,8 +10,107 @@ namespace Engine
 	}
 
 	Vector3::Vector3( float i_x, float i_y, float i_z )
-		:m_x( i_x ), m_y( i_y ), m_z( i_z )
+		:x( i_x ), y( i_y ), z( i_z )
 	{
+	}
+
+	Vector3::Vector3( const Vector3 & i_other )
+		: x( i_other.x ), y( i_other.y ), z( i_other.z )
+	{
+	}
+
+	void Vector3::operator=( const Vector3 & i_other )
+	{
+		this->x = i_other.x;
+		this->y = i_other.y;
+		this->z = i_other.z;
+	}
+
+	Vector3 & Vector3::operator+( const Vector3 & i_other )
+	{
+		this->x += i_other.x;
+		this->y += i_other.y;
+		this->z += i_other.z;
+		return *this;
+	}
+
+	void Vector3::operator+=( const Vector3 & i_other )
+	{
+		this->x += i_other.x;
+		this->y += i_other.y;
+		this->z += i_other.z;
+	}
+
+	Vector3 & Vector3::operator-( const Vector3 & i_other )
+	{
+		this->x -= i_other.x;
+		this->y -= i_other.y;
+		this->z -= i_other.z;
+		return *this;
+	}
+
+	void Vector3::operator-=( const Vector3 & i_other )
+	{
+		this->x -= i_other.x;
+		this->y -= i_other.y;
+		this->z -= i_other.z;
+	}
+
+	Vector3 & Vector3::operator*( float value )
+	{
+		this->x *= value;
+		this->y *= value;
+		this->z *= value;
+		return *this;
+	}
+
+	void Vector3::operator*=( float value )
+	{
+		this->x *= value;
+		this->y *= value;
+		this->z *= value;
+	}
+
+	Vector3 & Vector3::operator/( float value )
+	{
+		assert( value != 0 );
+		this->x /= value;
+		this->y /= value;
+		this->z /= value;
+		return *this;
+	}
+
+	void Vector3::operator/=( float value )
+	{
+		assert( value != 0 );
+		this->x /= value;
+		this->y /= value;
+		this->z /= value;
+	}
+
+	float Vector3::Dot( const Vector3 & i_other )
+	{
+		return this->x * i_other.x + this->y * i_other.y + this->z * i_other.z;
+	}
+
+	Vector3 Vector3::Cross( const Vector3 & i_other )
+	{
+		return Vector3( 
+			this->y * i_other.z - this->z * i_other.y,
+			this->z * i_other.x - this->x * i_other.z,
+			this->x * i_other.y - this->y * i_other.x
+		);
+	}
+
+	float Vector3::Magnitude()
+	{
+		return sqrt( this->x * this->x + this->y * this->y + this->z * this->z );
+	}
+
+	float Vector3::Distance(const Vector3 & i_other)
+	{
+		Vector3 temp = *this - i_other;
+		return temp.Magnitude();
 	}
 
 	Vector3::~Vector3()
--- Engine/Vector3.h
+++ Engine/Vector3.h
@@ -6,10 +6,35 @@ namespace Engine
 	public:
 		Vector3();
 		Vector3( float i_x, float i_y, float i_z );
+		Vector3( const Vector3 & i_other );
+		void  operator=( const Vector3 & i_other );
+
+		Vector3 & operator+( const Vector3 & i_other );
+		void  operator+=( const Vector3 & i_other );
+
+		Vector3 & operator-( const Vector3 & i_other );
+		void operator-=( const Vector3 & i_other );
+
+		Vector3 & operator*( float value );
+		void operator*=( float value );
+
+		Vector3 & operator/( float value );
+		void operator/=( float value );
+		
+		float Dot( const Vector3 & i_other );
+
+		Vector3 Cross( const Vector3 & i_other );
+
+		float Magnitude();
+
+		float Distance( const Vector3 & i_other );
+
+		inline Vector3 Normalize(){ return Vector3( *this / this->Magnitude() ); };
+
 		~Vector3();
-	private:
-		float m_x;
-		float m_y;
-		float m_z;
+
+		float x;
+		float y;
+		float z;
 	};
 };
