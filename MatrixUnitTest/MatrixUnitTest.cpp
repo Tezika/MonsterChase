72eaf769afcb8cdc23eb001a0cf19b87f16faaa4
Tezika <tezikazhou@gmail.com>
2019-03-12 17:04:37 -0600

    Change the properties for MatrixUnitTest

--- MatrixUnitTest/MatrixUnitTest.cpp
+++ MatrixUnitTest/MatrixUnitTest.cpp
@@ -2,11 +2,11 @@
 //
 
 #include "pch.h"
-#include <iostream>
+#include "assert.h"
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
