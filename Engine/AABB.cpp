8ffcc4e226e765eb3b3723cd205c3ad57839ac22
Tezika <tezikazhou@gmail.com>
2019-04-02 22:19:28 -0600

    Engine: Finish the basic logic for AABB class

--- Engine/AABB.cpp
+++ Engine/AABB.cpp
@@ -1,1 +1,25 @@
 #include "stdafx.h"
+#include "AABB.h"
+
+namespace Engine
+{
+	AABB::AABB( const Point2D<float> & i_center, const Point2D<float> & i_extends ) :
+		center( i_center ),
+		extends( i_extends )
+	{
+
+	}
+
+	AABB::AABB( const AABB & i_other ) :
+		center( i_other.center ),
+		extends( i_other.extends )
+	{
+
+	}
+
+	void AABB::operator=( const AABB & i_other )
+	{
+		this->center = i_other.center;
+		this->extends = i_other.extends;
+	}
+}
--- Engine/AABB.h
+++ Engine/AABB.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "Point2D.h"
+
+namespace Engine
+{
+	struct AABB
+	{
+	public:
+		AABB(){};
+		~AABB(){};
+		AABB( const Point2D<float> & i_center, const Point2D<float> & i_extends );
+		AABB( const AABB & i_other );
+		void operator=( const AABB & i_other );
+
+		Point2D<float> center;
+		Point2D<float> extends;
+	};
+}
