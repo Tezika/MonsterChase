9c752fce46248af2277afe199a2b0eb4ae5afdd9
Tezika <tezikazhou@gmail.com>
2019-04-27 11:49:59 -0600

    Messaging: Add 'RegisterMessageHandler' in.

--- Engine/MessagingSystem.cpp
+++ Engine/MessagingSystem.cpp
@@ -1,10 +1,13 @@
 #include "stdafx.h"
 #include "MessagingSystem.h"
+#include "TString.h"
 
 namespace Engine
 {
 	namespace MessagingSystem
 	{
-
+		void MessagingSystem::RegisterMessageHandler( const TString & i_Message, MessageHandler_t i_pHandler )
+		{
+		}
 	}
 }
--- Engine/MessagingSystem.h
+++ Engine/MessagingSystem.h
@@ -2,10 +2,12 @@
 
 namespace Engine
 {
+	// Use the TString class as a temporary solution. It'll change into the HashedString soon.
 	class TString;
 	namespace MessagingSystem
 	{
-		typedef void( *MessageHandler_t )( const TString & i_str );
+		typedef void( *MessageHandler_t )( const TString & i_Message );
 
+		void RegisterMessageHandler( const TString & i_Message, MessageHandler_t i_pHandler );
 	}
 }
