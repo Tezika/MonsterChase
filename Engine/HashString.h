532fb62ed52916d6b29ab2bdaff9edc253d279bc
Tezika <tezikazhou@gmail.com>
2019-04-28 22:11:50 -0600

    Messaging: Add HashedString logic in.

--- Engine/HashString.h
+++ Engine/HashString.h
@@ -1,7 +1,37 @@
-#pragma once
+﻿#pragma once
 namespace Engine
 {
-	class HashString
+	class HashedString
 	{
+	public:
+		HashedString();
+		~HashedString();
+
+		HashedString( const char * i_str );
+		HashedString( const HashedString & i_other );
+		HashedString( const HashedString && i_other );
+
+		HashedString & operator=( const HashedString & i_other );
+		HashedString & operator=( const HashedString && i_other );
+
+		unsigned int Get();
+
+#ifdef DEBUG_KEEP_STRING
+		inline const char * GetDebugString(){ return m_pStr; };
+#endif // DEBUG_KEEP_STRING
+
+		static unsigned int Hash( const char * i_str );
+		static unsigned int Hash( const void * i_bytes, size_t i_byteCount );
+
+		bool operator==( const HashedString & i_other ) const;
+		// Below for STL container compatibility. That's why I cannot use the TString;
+		bool operator<( const HashedString & i_other ) const;
+
+	private:
+		unsigned int m_Hash;
+
+#ifdef DEBUG_KEEP_STRING
+		const char * m_pStr;
+#endif
 	};
 }
