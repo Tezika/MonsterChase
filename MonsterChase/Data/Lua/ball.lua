e313436a52971f648693c73bde0a599e32a030cb
Tezika <tezikazhou@gmail.com>
2019-05-09 19:57:10 +0800

    FinalProject: Modify the ball's lua file.

--- MonsterChase/Data/Lua/ball.lua
+++ MonsterChase/Data/Lua/ball.lua
@@ -1,8 +1,8 @@
 gameObject = 
 {
-    name = "BadGuy",
-    initial_position = {x=200,y=0},
-    initial_velocity = {x=-100,y=0},
+    name = "Ball",
+    initial_position = {x=0,y=0},
+    initial_velocity = {x=0,y=0},
     physics_settings = {
         mass = 1,
         force = {x=0, y=0},
