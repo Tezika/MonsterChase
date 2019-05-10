0d8944dd24475fcf9cb1a8b531b228c87d3cb388
Tezika <tezikazhou@gmail.com>
2019-05-10 15:20:44 +0800

    MessageSystem: Refactor the delegate to handle one parameter for void *

--- Engine/MessageSystem.cpp
+++ Engine/MessageSystem.cpp
@@ -8,13 +8,13 @@ namespace Engine
 {
 	namespace Messaging
 	{
-		void MessageSystem::RegisterMessageDelegate( const HashedString & i_Message, Delegate<> & i_Delegate )
+		void MessageSystem::RegisterMessageDelegate( const HashedString & i_Message, Delegate<void *> & i_Delegate )
 		{
-			std::map<HashedString, MultiCastDelegate<>>::iterator itr;
+			std::map<HashedString, MultiCastDelegate<void *>>::iterator itr;
 			itr = m_pMessage2Delegates->find( i_Message );
 			if ( itr == m_pMessage2Delegates->end() )
 			{
-				MultiCastDelegate<> newMultiCastDelegate;
+				MultiCastDelegate<void *> newMultiCastDelegate;
 				newMultiCastDelegate.AddDelegate( i_Delegate );
 				m_pMessage2Delegates->insert( std::make_pair( i_Message, newMultiCastDelegate ) );
 			}
@@ -24,9 +24,9 @@ namespace Engine
 			}
 		}
 
-		void MessageSystem::DeregisterMessageDelegate( const HashedString & i_Message, Delegate<> & i_Delegate )
+		void MessageSystem::DeregisterMessageDelegate( const HashedString & i_Message, Delegate<void *> & i_Delegate )
 		{
-			std::map<HashedString, MultiCastDelegate<>>::iterator itr;
+			std::map<HashedString, MultiCastDelegate<void *>>::iterator itr;
 			itr = m_pMessage2Delegates->find( i_Message );
 			if ( itr == m_pMessage2Delegates->end() )
 			{
@@ -38,19 +38,19 @@ namespace Engine
 			}
 		}
 
-		void MessageSystem::SendMessageW( const HashedString & i_Message )
+		void MessageSystem::SendMessageW( const HashedString & i_Message, void * i_pInfo )
 		{
-			std::map<HashedString, MultiCastDelegate<>>::iterator itr;
+			std::map<HashedString, MultiCastDelegate<void *>>::iterator itr;
 			itr = m_pMessage2Delegates->find( i_Message );
 			if ( itr != m_pMessage2Delegates->end() )
 			{
-				itr->second.ExecuteIfBound();
+				itr->second.ExecuteIfBound( i_pInfo );
 			}
 		}
 
 		bool MessageSystem::Initialize()
 		{
-			m_pMessage2Delegates = new std::map<HashedString, MultiCastDelegate<>>();
+			m_pMessage2Delegates = new std::map<HashedString, MultiCastDelegate<void *>>();
 			assert( m_pMessage2Delegates != nullptr );
 			return true;
 		}
--- Engine/MessageSystem.h
+++ Engine/MessageSystem.h
@@ -7,8 +7,7 @@ namespace Engine
 {
 	class TString;
 	class HashedString;
-	// Use the TString class as a temporary solution. It'll change into the HashedString soon.
-	// It only supports the method with 0 augment right now.
+
 	namespace Messaging
 	{
 		class MessageSystem
@@ -24,16 +23,15 @@ namespace Engine
 			MessageSystem( MessageSystem const& ) = delete;
 			void operator=( MessageSystem const& ) = delete;
 
-			void RegisterMessageDelegate( const HashedString & i_Message, Delegate<> & i_Delegate );
-			void DeregisterMessageDelegate( const HashedString & i_Message, Delegate<> & i_Delegate );
-			void SendMessageW( const HashedString & i_Message );
+			void RegisterMessageDelegate( const HashedString & i_Message, Delegate<void *> & i_Delegate );
+			void DeregisterMessageDelegate( const HashedString & i_Message, Delegate< void *> & i_Delegate );
+			void SendMessageW( const HashedString & i_Message, void * info );
 
 			bool Initialize();
 			bool Destroy();
 
-			
 		private:
-			std::map<HashedString, MultiCastDelegate<>> * m_pMessage2Delegates;
+			std::map<HashedString, MultiCastDelegate<void *>> * m_pMessage2Delegates;
 			MessageSystem(){};
 		};
 	}
