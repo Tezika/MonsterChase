48be764bff601abe06ea9eaad9797e1c2169f1d2
Tezika <tezikazhou@gmail.com>
2019-08-19 17:13:09 -0600

    Formatted the code a little bit.

--- Engine/PhysicsInfo.h
+++ Engine/PhysicsInfo.h
@@ -19,29 +19,29 @@ namespace Engine
 			PhysicsInfo( const PhysicsInfo & );
 			void operator=( const PhysicsInfo & );
 
-			inline SmartPtr<GameObject> GetGameObject() const{ return m_pGo; }
+			inline SmartPtr<GameObject> GetGameObject() const { return m_pGo; }
 
-			inline const Vector3SSE & GetDrivingForce(){ return m_force; }
-			inline void SetDrivingForce( const Vector3SSE & i_oForce ){ m_force = i_oForce; };
+			inline const Vector3SSE & GetDrivingForce() { return m_force; }
+			inline void SetDrivingForce( const Vector3SSE & i_oForce ) { m_force = i_oForce; };
 
 			inline float GetMass() const { return m_mass; }
 			inline float GetDragness() const { return m_dragness; }
 
-			inline AABB * GetAABB(){ return m_pAABB; }
-			inline void SetAABB( AABB * i_pAABB ){ m_pAABB = i_pAABB; }
+			inline AABB * GetAABB() { return m_pAABB; }
+			inline void SetAABB( AABB * i_pAABB ) { m_pAABB = i_pAABB; }
 
-			inline bool GetIsCollision(){ return m_bIsCollision; };
-			inline void SetIsCollision( bool i_inCollision ){ m_bIsCollision = i_inCollision; };
+			inline bool GetIsCollision() { return m_bIsCollision; };
+			inline void SetIsCollision( bool i_inCollision ) { m_bIsCollision = i_inCollision; };
 
 			inline bool GetCollidable() { return m_bCollidable; }
-			inline void SetCollidable( bool i_bCollidable ){ m_bCollidable = i_bCollidable; }
+			inline void SetCollidable( bool i_bCollidable ) { m_bCollidable = i_bCollidable; }
 
-			inline bool IsReflectable(){ return m_bReflectable; }
-			inline void SetReflectable( bool i_bReflectable ){ m_bReflectable = i_bReflectable; }
+			inline bool IsReflectable() { return m_bReflectable; }
+			inline void SetReflectable( bool i_bReflectable ) { m_bReflectable = i_bReflectable; }
 
 			void OnCollision();
 		private:
-			PhysicsInfo(){};
+			PhysicsInfo() {};
 			PhysicsInfo( float i_mass, float i_drag, bool i_bCollidable, bool i_bReflectable, SmartPtr<GameObject> i_pGo, AABB * i_pAABB );
 			SmartPtr<GameObject> m_pGo;
 			Vector3SSE m_force;
